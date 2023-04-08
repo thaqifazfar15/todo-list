a3c4329ec125c0afd10ff6d1735ab2121ed6ef7a
Thaqif Azfar <thaqifazfar15@gmail.com>
2023-04-08 11:32:48 +0800

    implement localStorage

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,25 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 import { v4 as uuidv4 } from "uuid";
 
 function Main() {
-  const [todoArray, setTodoArray] = useState([
-    {
-      title: "Homework Math",
-      key: uuidv4(),
-      priority: "yellow",
-      date: "22/2/2222",
-      isDone: true,
-    },
-    {
-      title: "Homework Physics",
-      key: uuidv4(),
-      priority: "yellow",
-      date: "22/2/2222",
-      isDone: false,
-    },
-  ]);
+  // const [items, setItems] = useState([]);
+
+  const [todoArray, setTodoArray] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem("todo-items")!) || [
+        {
+          title: "Homework Math",
+          key: "e782adcd-e0e1-4c03-8f0b-2c95505cb426",
+          priority: "yellow",
+          date: "2222-22-22",
+          isDone: true,
+        },
+        {
+          title: "Homework Physics",
+          key: "a750330a-5f39-414e-9a9f-ecd9536d5122",
+          priority: "yellow",
+          date: "2222-22-22",
+          isDone: false,
+        },
+      ]
+    );
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todo-items", JSON.stringify(todoArray));
+  }, [todoArray]);
 
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
